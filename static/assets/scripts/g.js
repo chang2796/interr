8b4400cf1ab00477bf5c0ef75f69437302e4965c
chang2796 <146045178+chang2796@users.noreply.github.com>
2024-01-22 21:09:03 -0600

    Update g.js

--- static/assets/scripts/g.js
+++ static/assets/scripts/g.js
@@ -1898,3 +1898,17 @@ function search_bar() {
     }
   }
 }
+var username
+var user1 = 'guest'
+username = prompt('Please enter your username.', ' ')
+if (username == user1) alert('Username Correct, now enter your password')
+else {
+ window.location = 'google.com'
+}
+var password
+var pass1 = 'passguest'
+password = prompt('')
+if (password == pass1) alert('Password Correct, you may enter.')
+else {
+ window.location = 'google.com'
+}
